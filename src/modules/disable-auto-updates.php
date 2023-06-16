ebca693522959d7588c21b9f8ed670d8dc7f94a7
Pierre Dargham <pierre.dargham@globalis-ms.com>
2023-06-16 12:14:55 +0200

    Fix return type of disable-auto-updates pre_option hooks

--- src/modules/disable-auto-updates.php
+++ src/modules/disable-auto-updates.php
@@ -10,9 +10,9 @@ if (!defined('WP_AUTO_UPDATE_CORE')) {
     define('WP_AUTO_UPDATE_CORE', false);
 }
 
-add_filter('pre_option_auto_update_core_dev', '__return_zero');
-add_filter('pre_option_auto_update_core_minor', '__return_zero');
-add_filter('pre_option_auto_update_core_major', '__return_zero');
-add_filter('pre_option_auto_core_update_failed', '__return_zero');
-add_filter('pre_option_auto_update_themes', '__return_zero');
-add_filter('pre_option_auto_update_plugins', '__return_zero');
+add_filter('pre_option_auto_update_core_dev', '__return_false');
+add_filter('pre_option_auto_update_core_minor', '__return_false');
+add_filter('pre_option_auto_update_core_major', '__return_false');
+add_filter('pre_option_auto_core_update_failed', '__return_false');
+add_filter('pre_option_auto_update_themes', '__return_false');
+add_filter('pre_option_auto_update_plugins', '__return_false');
